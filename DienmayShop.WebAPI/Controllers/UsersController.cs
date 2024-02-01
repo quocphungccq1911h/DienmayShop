11517f17b07206927f7e7d144ec0e9c8c4fc4365
quocphungcn80 <quocphungcn80@gmail.com>
2024-02-01 22:10:17 +0700

    vdsvd

--- DienmayShop.WebAPI/Controllers/UsersController.cs
+++ DienmayShop.WebAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using DienmayShop.Application.System.Users;
+using DienmayShop.ViewModel.Common;
 using DienmayShop.ViewModel.System.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -22,37 +23,77 @@ namespace DienmayShop.WebAPI.Controllers
         #endregion
 
         #region Methods
+
+        #region Authenticate
         [HttpPost("Authenticate")]
         [AllowAnonymous]
         public async Task<IActionResult> Authenticate([FromBody] LoginRequest loginRequest)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             var resultToken = await _userService.Authencate(loginRequest);
-            if(resultToken == null)
+            if (resultToken == null)
             {
                 return BadRequest(resultToken);
             }
             return Ok(resultToken);
         }
+        #endregion
 
+        #region Register
         [HttpPost("Register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register([FromBody]  RegisterRequest registerRequest)
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             var response = await _userService.Register(registerRequest);
-            if(!response.IsSuccessed)
+            if (!response.IsSuccessed)
             {
                 return BadRequest(response.Message);
             }
             return Ok(response);
         }
+        #endregion
+
+        #region DeleteUser
+        [HttpDelete("DeleteUserWithId")]
+        public async Task<IActionResult> DeleteUserById(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest("Tài khoản không hợp lệ. Vui lòng kiểm tra lại");
+            }
+            ApiResult<bool> response = await _userService.DeleteUser(id);
+            return Ok(response);
+
+        }
+        #endregion
+
+        #region UpdateUser
+        public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UserUpdateRequest request)
+        {
+            if(id == Guid.Empty)
+            {
+                return BadRequest("Tài khoản không tồn tại. Vui lòng kiểm tra lại");
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var response = await _userService.UpdateUser(id, request);
+            if(!response.IsSuccessed)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        #endregion
+
         #endregion
     }
 }
