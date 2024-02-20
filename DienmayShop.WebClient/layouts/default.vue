410153bdf7fccb3f7b4eb9d12466f4284f70f6bd
quocphungcn80 <quocphungcn80@gmail.com>
2024-02-20 20:59:49 +0700

    vds

---
+++ DienmayShop.WebClient/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <section>
+            xin chao
+        </section>
+        <section>
+            <Nuxt />
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
